6fc2532914c9cf8330680b56e5cd936621b3a782
Axel Diaz <axtherocknass@gmail.com>
2022-02-03 11:44:35 -0600

    Haciendo la peticiones y creando el inner HTML

---
+++ static/js/api.js
@@ -0,0 +1,7 @@
+export default class API {
+    getCharacter(id) {
+        fetch(`https://rickandmortyapi.com/api/character/${id}`)
+            .then((response) => response.json())
+            .then((data) => console.log(data))
+    }
+}
--- static/js/app.js
+++ static/js/app.js
@@ -1,1 +1,26 @@
-console.log("Hola que hace ")
+import API from './api.js'
+const api = new API()
+console.log("HOla")
+
+const $characterContainer = document.querySelector('#character-container')
+
+const characterTemplate = `
+<article class="character">
+    <div class="character-grid">
+        <h2>Rick Sanchez</h2>
+        <img src="" alt="">
+    </div>
+</article>
+`
+
+
+
+class Character {
+    render() {
+        $characterContainer.innerHTML = characterTemplate
+    }
+}
+
+const rick = new Character()
+rick.render()
+console.log(api.getCharacter(2))
